983d16f8ba1f2d0a44b17220fe7cdd452823e009
Eyup Dogan <eyodohan@gmail.com>
2022-05-18 22:31:49 +0300

    added nesting navigators

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function FavoritesScreen() {
+  return <Text>The favorites screen</Text>;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default FavoritesScreen;
